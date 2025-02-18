9114703cc8960b1eb4f23e166478983ce1c923e9
gyuri00 <skarbfl0802@gmail.com>
2025-02-18 20:18:16 +0900

    init

---
+++ src/main/java/com/GDG/Festi/FestiApplication.java
@@ -0,0 +1,13 @@
+package com.GDG.Festi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GDG/Festi/FestiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GDG.Festi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
